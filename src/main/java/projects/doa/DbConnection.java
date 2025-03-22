0b09227d86a7d78096e926b3086f90b10315cfe2
Angel <romeroangel820@gmail.com>
2025-03-21 21:46:13 -0700

    first commit

---
+++ src/main/java/projects/doa/DbConnection.java
@@ -0,0 +1,5 @@
+package projects.doa;
+
+public class DbConnection {
+
+}
---
+++ src/main/java/projectsApp/ProjectApp.java
@@ -0,0 +1,10 @@
+package projectsApp;
+
+public class ProjectApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
