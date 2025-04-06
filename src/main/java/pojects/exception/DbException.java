4cbb367e1a5d9768b62a94f608ecdba48b672067
Angel <romeroangel820@gmail.com>
2025-04-05 17:37:45 -0700

    your message for second commit

--- src/main/java/pojects/exception/DbException.java
+++ src/main/java/pojects/exception/DbException.java
@@ -3,23 +3,18 @@ package pojects.exception;
 @SuppressWarnings("serial")
 public class DbException extends RuntimeException {
 
-	public DbException() {
-		
-	}
 
-	public DbException(String message) {
-		super(message);
-		
-	}
+  public DbException(String message) {
+    super(message);
+  }
 
-	public DbException(Throwable cause) {
-		super(cause);
-		
-	}
 
-	public DbException(String message, Throwable cause) {
-		super(message, cause);
-		
-	}
+  public DbException(Throwable cause) {
+    super(cause);
+  }
 
+
+  public DbException(String message, Throwable cause) {
+    super(message, cause);
+  }
 }
